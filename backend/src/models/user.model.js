58a518d7453848437bafe2297946846b67010b73
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-04-30 19:15:53 +0500

    Update user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -11,23 +11,32 @@ const User = sequelize.define(
       primaryKey: true,
     },
     firstName: {
-      type: DataTypes.STRING(30),
+      type: DataTypes.STRING,
       allowNull: false,
     },
     lastName: {
-      type: DataTypes.STRING(30),
+      type: DataTypes.STRING,
       allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.STRING,
     },
     dateOfBirth: {
       type: DataTypes.DATE,
       allowNull: false,
+    },
+    About: {
+      type: DataTypes.TEXT,
+      validate: {
+        max: 150,
+      },
     },
     phoneNumber: {
       type: DataTypes.STRING(11),
       unique: true,
     },
     email: {
-      type: DataTypes.STRING(30),
+      type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
