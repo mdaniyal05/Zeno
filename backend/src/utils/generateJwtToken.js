c9e16ca015b8a9478e51f75d3c9c79176993c542
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-01 17:03:04 +0500

    Create generate jwt token function

---
+++ backend/src/utils/generateJwtToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const generateJwtToken = (res, userId) => {
+  const jwtToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "20h",
+  });
+
+  res.cookie("jwtToken", jwtToken, {
+    httpOnly: true,
+    maxAge: 3600000,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
+};
+
+module.exports = generateJwtToken;
