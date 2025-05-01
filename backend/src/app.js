918318bcbeae6f539a7cb396c92966c339ce8a5e
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-01 16:53:18 +0500

    Use auth route and middleware in express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/auth.route");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
