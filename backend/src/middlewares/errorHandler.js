f5f90fe1b0d46b77c83de8252409cf8f284f54ca
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-03 19:56:56 +0500

    Create custom error handler middleware

---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,20 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`${req.originalUrl} - Not Found`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const message = err.message;
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
