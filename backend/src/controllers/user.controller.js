2eaf425dc0174f34e749f215de65131105beacc8
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-09 08:32:03 +0500

    Create get user profile controller function

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,24 @@
-const getUserProfile = (req, res) => {
-  res.status(200).json({
-    message: "User profile.",
-  });
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user.model");
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findByPk(req.user.userId);
+
+  if (user) {
+    res.status(200).json({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      avatar: user.avatar,
+      dateOfBirth: user.dateOfBirth,
+      about: user.about,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found.");
+  }
+});
 
 const updateUserProfile = (req, res) => {
   res.status(201).json({
