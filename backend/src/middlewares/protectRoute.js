4e1aa7b9a47ab86e26f54c61ab6ff05da7813630
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-09 08:14:36 +0500

    Create protect route middleware function

---
+++ backend/src/middlewares/protectRoute.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const protectRoute = asyncHandler(async (req, res, next) => {
+  let jwtToken;
+
+  jwtToken = req.cookies.jwtToken;
+
+  if (jwtToken) {
+    try {
+      const verifyUser = jwt.verify(jwtToken, process.env.JWT_SECRET);
+      req.user = await User.findByPk(verifyUser.userId).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not Authorized. Token Failed.");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not Authorized. No Token.");
+  }
+});
+
+module.exports = protectRoute;
