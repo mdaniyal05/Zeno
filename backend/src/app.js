8f43b742fc43d535dd5a8efcf44e93e7722e0ff2
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-09 08:18:59 +0500

    Use error handler middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.route");
 const userRoutes = require("./routes/user.route");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +13,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(notFound());
+app.use(errorHandler());
 
 module.exports = app;
