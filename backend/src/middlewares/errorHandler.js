176c1509d5bcfbe36f41fb809142dbcf595e35a4
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-10 20:44:24 +0500

    Fix

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -5,8 +5,8 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  const message = err.message;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
 
   res.status(statusCode).json({
     message: message,
