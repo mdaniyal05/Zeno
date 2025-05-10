edda324d9f95044a5884246d901198e2359317c2
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-10 20:48:40 +0500

    Handle sequelize and sql related errors

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -8,6 +8,29 @@ const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
+  if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    statusCode = 400;
+    message = err.errors.map((e) => e.message).join(", ");
+  } else if (err.name === "SequelizeEmptyResultError") {
+    statusCode = 404;
+    message = "Resource Not Found";
+  } else if (err.name === "SequelizeForeignKeyConstraintError") {
+    statusCode = 400;
+    message = "Foreign key constraint violation";
+  } else if (
+    err.name === "SequelizeConnectionError" ||
+    err.name === "SequelizeConnectionRefusedError"
+  ) {
+    statusCode = 503;
+    message = "Database connection error";
+  } else if (err.code === "EREQUEST") {
+    statusCode = 400;
+    message = "Invalid SQL request";
+  }
+
   res.status(statusCode).json({
     message: message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
