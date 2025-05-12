ae05e01449b36b6842ff01237f1f8ba04fa6f085
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-12 07:33:26 +0500

    Add more attributes to account model

--- backend/src/models/account.model.js
+++ backend/src/models/account.model.js
@@ -1,11 +1,58 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../db/db");
+const User = require("./user.model");
 
-const Account = sequelize.define("Account", {
-  accountId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
+const Account = sequelize.define(
+  "Account",
+  {
+    accountId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "userId",
+      },
+    },
+    accountName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountBalance: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    accountCurrency: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+    },
+    bankName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
   },
-});
+  {
+    tableName: "Accounts",
+    timestamps: true,
+  }
+);
+
+Account.sync();
+
+module.exports = Account;
