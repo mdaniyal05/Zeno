c5a26f6283e286b2e9f5af4bfba1769fbb1d839c
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-13 15:04:21 +0500

    Modify transaction model

--- backend/src/models/transaction.model.js
+++ backend/src/models/transaction.model.js
@@ -3,55 +3,54 @@ const sequelize = require("../db/db");
 const User = require("./user.model");
 const Category = require("./category.model");
 
-const Transaction = sequelize.define("Transaction", {
-  transactionId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  user: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: User,
-      key: "userId",
+const Transaction = sequelize.define(
+  "Transaction",
+  {
+    transactionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "userId",
+      },
+    },
+    category: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: "categoryId",
+      },
+    },
+    transactionAmount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    transactionType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    paymentMethod: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    notes: {
+      type: DataTypes.TEXT,
     },
   },
-  category: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Category,
-      key: "categoryId",
-    },
-  },
-  transactionDate: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  transactionTime: {
-    type: DataTypes.TIME,
-    allowNull: false,
-  },
-  transactionAmount: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  transactionType: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  paymentMethod: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  notes: {
-    type: DataTypes.TEXT,
-  },
-});
+  {
+    tableName: "Transactions",
+    createdAt: true,
+  }
+);
 
 Transaction.sync();
 
