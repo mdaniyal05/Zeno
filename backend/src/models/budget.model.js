a32ebf64b0253c0d2d7720e8e4383fab8526e4c9
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-13 15:36:05 +0500

    Add more attributes to budget model + Add timestamps to category model

--- backend/src/models/budget.model.js
+++ backend/src/models/budget.model.js
@@ -1,14 +1,60 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../db/db");
+const User = require("./user.model");
+const Category = require("./category.model");
 
-const Budget = sequelize.define("Budget", {
-  budgetId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
+const Budget = sequelize.define(
+  "Budget",
+  {
+    budgetId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "userId",
+      },
+    },
+    category: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: "categoryId",
+      },
+    },
+    budgetName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    budgetAmount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    budgetPeriod: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   },
-});
+  {
+    tableName: "Budgets",
+    timestamps: true,
+  }
+);
 
 Budget.sync();
 
--- backend/src/models/category.model.js
+++ backend/src/models/category.model.js
@@ -2,40 +2,47 @@ const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../db/db");
 const User = require("./user.model");
 
-const Category = sequelize.define("Category", {
-  categoryId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  user: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: User,
-      key: "userId",
+const Category = sequelize.define(
+  "Category",
+  {
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "userId",
+      },
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    categoryType: {
+      type: DataTypes.ENUM("Expense", "Income"),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    monthlyLimit: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
     },
   },
-  categoryName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  categoryType: {
-    type: DataTypes.ENUM("Expense", "Income"),
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-  },
-  monthlyLimit: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "Categories",
+    timestamps: true,
+  }
+);
 
 Category.sync();
 
