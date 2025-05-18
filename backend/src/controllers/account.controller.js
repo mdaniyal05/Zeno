36744602e78d76c91d5bff506841e1ac7a72f499
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-18 19:36:43 +0500

    Add logic to get user account controller

--- backend/src/controllers/account.controller.js
+++ backend/src/controllers/account.controller.js
@@ -2,9 +2,23 @@ const asyncHandler = require("express-async-handler");
 const Account = require("../models/account.model");
 
 const getUserAccount = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "User Account",
-  });
+  const accountId = req.params.id;
+  const account = await Account.findByPk(accountId);
+
+  if (account) {
+    res.status(200).json({
+      accountName: account.accountName,
+      accountType: account.accountType,
+      accountBalance: account.accountBalance,
+      accountCurrency: account.accountCurrency,
+      bankName: account.bankName,
+      accountNumber: account.accountNumber,
+      isActive: account.isActive,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Account Not Found.");
+  }
 });
 
 const getAllUserAccounts = asyncHandler(async (req, res) => {
