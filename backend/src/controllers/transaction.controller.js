83edaa5f924b32be0a56d8d2258b9c6082d456e4
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-23 14:19:13 +0500

    Add logic to transaction controller function

--- backend/src/controllers/transaction.controller.js
+++ backend/src/controllers/transaction.controller.js
@@ -2,9 +2,22 @@ const asyncHandler = require("express-async-handler");
 const Transaction = require("../models/transaction.model");
 
 const getTransaction = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Transaction",
-  });
+  const transactionId = req.params.id;
+  const transaction = await Transaction.findByPk(transactionId);
+
+  if (transaction) {
+    res.status(200).json({
+      transactionAmount: transaction.transactionAmount,
+      transactionType: transaction.transactionType,
+      paymentMethod: transaction.paymentMethod,
+      description: transaction,
+      description,
+      notes: transaction.notes,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Transaction Not Found.");
+  }
 });
 
 const getAllTransactions = asyncHandler(async (req, res) => {
