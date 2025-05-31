e3c8fe38d7db930bd5f8c5cc5d6ce4ef39d8fdeb
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-31 22:06:53 +0500

    Add logic to create budget controller function + Again do not allow null

--- backend/src/controllers/budget.controller.js
+++ backend/src/controllers/budget.controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Budget = require("../models/budget.model");
+const Category = require("../models/category.model");
 
 const getBudget = asyncHandler(async (req, res) => {
   const budgetId = req.params.id;
@@ -45,9 +46,51 @@ const getAllBudgets = asyncHandler(async (req, res) => {
 });
 
 const createBudget = asyncHandler(async (req, res) => {
-  res.status(201).json({
-    message: "Budget Created",
+  const {
+    budgetAmount,
+    budgetPeriod,
+    description,
+    startDate,
+    endDate,
+    categoryName,
+  } = req.body;
+
+  const category = await Category.findOne({
+    where: { categoryName: categoryName },
   });
+  const userId = req.user.userId;
+  const notificationsEnabled = true;
+  const status = "Active";
+
+  const newBudget = await Budget.create({
+    budgetAmount: budgetAmount,
+    budgetPeriod: budgetPeriod,
+    description: description,
+    startDate: startDate,
+    endDate: endDate,
+    amountSpent: 0,
+    amountRemaining: budgetAmount,
+    percentUsed: 0,
+    status: status,
+    notificationsEnabled: notificationsEnabled,
+    userId: userId,
+    categoryId: category.categoryId,
+  });
+
+  if (newBudget) {
+    res.status(201).json({
+      budgetAmount: newBudget.budgetAmount,
+      budgetPeriod: newBudget.budgetPeriod,
+      startDate: newBudget.startDate,
+      endDate: newBudget.endDate,
+      status: newBudget.status,
+      notificationsEnabled: newBudget.notificationsEnabled,
+      message: "Budget Created Successfully.",
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Budget Data.");
+  }
 });
 
 const deleteBudget = asyncHandler(async (req, res) => {
