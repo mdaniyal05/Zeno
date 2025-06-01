0e000f963061aae31dce4635af075b6694d24f79
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-01 17:35:26 +0500

    Sync income and expense models

--- backend/src/db/syncModels.js
+++ backend/src/db/syncModels.js
@@ -3,6 +3,8 @@ const Account = require("../models/account.model");
 const Category = require("../models/category.model");
 const Transaction = require("../models/transaction.model");
 const Budget = require("../models/budget.model");
+const Income = require("../models/income.model");
+const Expense = require("../models/expense.model");
 
 const syncModels = () => {
   User.sync();
@@ -10,6 +12,8 @@ const syncModels = () => {
   Category.sync();
   Transaction.sync();
   Budget.sync();
+  Income.sync();
+  Expense.sync();
 };
 
 module.exports = syncModels;
