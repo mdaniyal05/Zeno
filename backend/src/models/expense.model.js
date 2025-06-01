67c6056c756c5c4d03ae316889dacd1e0d9e6412
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-01 16:03:13 +0500

    Create expense model

---
+++ backend/src/models/expense.model.js
@@ -0,0 +1,46 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../db/db");
+const User = require("./user.model");
+
+const Expense = sequelize.define(
+  "Expense",
+  {
+    expenseId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    expenseAmount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    currency: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+    },
+    expenseDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    merchant: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "Expenses",
+    timestamps: true,
+    updatedAt: false,
+  }
+);
+
+User.hasMany(Expense, {
+  foreignKey: { name: "userId", allowNull: false },
+});
+Expense.belongsTo(User, {
+  as: "owner",
+  foreignKey: { name: "userId" },
+});
+
+module.exports = Expense;
