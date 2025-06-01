cc3c6c5601d93257cbb00ea9a86a4fa3426ff361
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-01 15:46:40 +0500

    Create income model

---
+++ backend/src/models/income.model.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../db/db");
+const User = require("./user.model");
+
+const Income = sequelize.define(
+  "Income",
+  {
+    incomeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    incomeAmount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    currency: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+    },
+    incomeDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    incomeSource: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { tableName: "Incomes", timestamps: true, updatedAt: false }
+);
+
+User.hasMany(Income, {
+  foreignKey: { name: "userId", allowNull: false },
+});
+Income.belongsTo(User, {
+  as: "owner",
+  foreignKey: { name: "userId" },
+});
+
+module.exports = Income;
