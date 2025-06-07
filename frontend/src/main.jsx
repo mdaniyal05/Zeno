0a61048775d7da3455e12cf950a48ee421b71db5
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-07 20:08:45 +0500

    Fix rendering issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<LandingPage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
