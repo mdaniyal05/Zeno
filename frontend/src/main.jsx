70512c2f6bfcabe47753934b2b0c47b1700b3ad4
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-07 20:19:02 +0500

    Add routes for sign up and sign in + Fix error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,12 +10,17 @@ import "./index.css";
 import App from "./App.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import SignUp from "./pages/SignUpPage.jsx";
+import SignIn from "./pages/SignInPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/signin" element={<SignIn />} />
     </Route>
   )
 );
