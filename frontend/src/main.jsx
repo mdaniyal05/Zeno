182d561df252b79954eb291327517b3a9ace98a4
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-09 04:02:07 +0500

    Wrap main file in provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,9 +10,10 @@ import "./index.css";
 import App from "./App.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import SignUp from "./pages/SignUpPage.jsx";
-import SignIn from "./pages/SignInPage.jsx";
+import SignInPage from "./pages/SignInPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,13 +21,15 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signin" element={<SignInPage />} />
     </Route>
   )
 );
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 );
