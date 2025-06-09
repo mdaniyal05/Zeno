92a1071bfa664aad06ac4d83a500cfca7988ea79
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-09 06:41:40 +0500

    Create budget endpoints with mutation

---
+++ frontend/src/redux/slices/budgetApiSlice.js
@@ -0,0 +1,40 @@
+import { apiSlice } from "./apiSlice";
+
+const BUDGET_URL = "/api/budgets";
+
+export const budgetApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllUserBudgets: builder.mutation({
+      query: () => ({
+        url: `${BUDGET_URL}/`,
+        method: "GET",
+      }),
+    }),
+    getUserBudget: builder.mutation({
+      query: () => ({
+        url: `${BUDGET_URL}/budget/:id`,
+        method: "GET",
+      }),
+    }),
+    createUserBudget: builder.mutation({
+      query: (data) => ({
+        url: `${BUDGET_URL}/create-budget`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    deleteUserBudget: builder.mutation({
+      query: () => ({
+        url: `${BUDGET_URL}/budget/:id`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetAllUserBudgetsMutation,
+  useGetUserBudgetMutation,
+  useCreateUserBudgetMutation,
+  useDeleteUserBudgetMutation,
+} = budgetApiSlice;
