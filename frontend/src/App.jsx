b705a5610269aa1ad21db4a7f64f8760b6f73386
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-10 15:54:56 +0500

    Clear local storage when the session is null and tab is closed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,18 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+function clearStorage() {
+  let session = sessionStorage.getItem("register");
+
+  if (session == null) {
+    localStorage.removeItem("userInfo");
+  }
+  sessionStorage.setItem("register", 1);
+}
+
 function App() {
+  window.addEventListener("load", clearStorage);
+
   return (
     <>
       <ToastContainer />
