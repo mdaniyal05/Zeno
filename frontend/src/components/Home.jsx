09c08c33e9a576e8be1e703e337f0ecd3936fcb2
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-06-14 05:05:10 +0500

    Calculate dashboard values

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,55 +1,49 @@
+import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import PageViewsBarChart from "./dashboard/components/PageViewsBarChart";
-import SessionsChart from "./dashboard/components/SessionsChart";
 import StatCard from "./dashboard/components/StatCard";
-
-const data = [
-  {
-    title: "Users",
-    value: "14k",
-    interval: "Last 30 days",
-    trend: "up",
-    data: [
-      200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340,
-      380, 360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
-    ],
-  },
-  {
-    title: "Conversions",
-    value: "325",
-    interval: "Last 30 days",
-    trend: "down",
-    data: [
-      1640, 1250, 970, 1130, 1050, 900, 720, 1080, 900, 450, 920, 820, 840, 600,
-      820, 780, 800, 760, 380, 740, 660, 620, 840, 500, 520, 480, 400, 360, 300,
-      220,
-    ],
-  },
-  {
-    title: "Event count",
-    value: "200k",
-    interval: "Last 30 days",
-    trend: "neutral",
-    data: [
-      500, 400, 510, 530, 520, 600, 530, 520, 510, 730, 520, 510, 530, 620, 510,
-      530, 520, 410, 530, 520, 610, 530, 520, 610, 530, 420, 510, 430, 520, 510,
-    ],
-  },
-  {
-    title: "Event count",
-    value: "200k",
-    interval: "Last 30 days",
-    trend: "neutral",
-    data: [
-      500, 400, 510, 530, 520, 600, 530, 520, 510, 730, 520, 510, 530, 620, 510,
-      530, 520, 410, 530, 520, 610, 530, 520, 610, 530, 420, 510, 430, 520, 510,
-    ],
-  },
-];
+import { useGetAllUserIncomesQuery } from "../redux/slices/incomeApiSlice";
+import { useGetAllUserExpensesQuery } from "../redux/slices/expenseApiSlice";
 
 export default function Home() {
+  const [incomes, setIncomes] = React.useState([]);
+  const [totalIncome, setTotalIncome] = React.useState(0);
+  const [expenses, setExpenses] = React.useState([]);
+  const [totalExpense, setTotalExpense] = React.useState(0);
+
+  const { data: income } = useGetAllUserIncomesQuery();
+  const { data: expense } = useGetAllUserExpensesQuery();
+
+  React.useEffect(() => {
+    let incomeAmounts = [];
+    let expenseAmounts = [];
+    let incomeTotal = 0;
+    let expenseTotal = 0;
+
+    if (income && expense) {
+      income.incomesData.map((income) =>
+        incomeAmounts.push(income.incomeAmount)
+      );
+      setIncomes(incomeAmounts);
+
+      income.incomesData.map(
+        (income) => (incomeTotal = incomeTotal + income.incomeAmount)
+      );
+      setTotalIncome(incomeTotal);
+
+      expense.expensesData.map((expense) =>
+        expenseAmounts.push(expense.expenseAmount)
+      );
+      setExpenses(expenseAmounts);
+
+      expense.expensesData.map(
+        (expense) => (expenseTotal = expenseTotal + expense.expenseAmount)
+      );
+      setTotalExpense(expenseTotal);
+    }
+  }, [income, expense]);
+
   return (
     <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
       {/* cards */}
@@ -62,18 +56,42 @@ export default function Home() {
         columns={12}
         sx={{ mb: (theme) => theme.spacing(2) }}
       >
-        {data.map((card, index) => (
-          <Grid key={index} size={{ xs: 12, sm: 6, lg: 3 }}>
-            <StatCard {...card} />
-          </Grid>
-        ))}
-        <Grid size={{ xs: 12, sm: 6, lg: 7 }}></Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SessionsChart />
+        <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+          <StatCard
+            title={"Incomes"}
+            value={totalIncome}
+            interval={"Last 30 Days"}
+            trend={"down"}
+            data={incomes}
+          />
         </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <PageViewsBarChart />
+        <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+          <StatCard
+            title={"Expenses"}
+            value={totalExpense}
+            interval={"Last 30 Days"}
+            trend={"up"}
+            data={expenses}
+          />
         </Grid>
+        {/* <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+          <StatCard
+            title={"Incomes"}
+            value={totalIncome}
+            interval={"Last 20 Days"}
+            trend={"down"}
+            data={incomes}
+          />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+          <StatCard
+            title={"Incomes"}
+            value={totalIncome}
+            interval={"Last 20 Days"}
+            trend={"down"}
+            data={incomes}
+          />
+        </Grid> */}
       </Grid>
     </Box>
   );
