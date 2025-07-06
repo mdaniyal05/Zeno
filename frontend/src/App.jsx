6da4ed69da2ef887a9338a165dbd932efcdad1e8
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-06 15:41:04 +0500

    Call logout mutation on load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,32 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-function clearStorage() {
-  let session = sessionStorage.getItem("register");
-
-  if (session == null) {
-    localStorage.removeItem("userInfo");
-  }
-  sessionStorage.setItem("register", 1);
-}
+import { useLogoutMutation } from "./redux/slices/authApiSlice";
+import { logout } from "./redux/slices/authSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const clearStorage = async () => {
+    let session = sessionStorage.getItem("register");
+
+    if (session == null) {
+      try {
+        await logoutApiCall().unwrap();
+        dispatch(logout());
+        navigate("/");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    sessionStorage.setItem("register", 1);
+  };
+
   window.addEventListener("load", clearStorage);
 
   return (
