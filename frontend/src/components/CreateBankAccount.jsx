a3d38c2b2cfbcf24e73ffcab4c1d34deab269b03
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-06 14:36:54 +0500

    Create form to create bank account

--- frontend/src/components/CreateBankAccount.jsx
+++ frontend/src/components/CreateBankAccount.jsx
@@ -1,5 +1,183 @@
-const CreateBankAccount = () => {
-  return <div></div>;
-};
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { styled } from "@mui/material/styles";
+import AppTheme from "./shared-theme/AppTheme";
+import ColorModeSelect from "./shared-theme/ColorModeSelect";
+import { Link } from "react-router-dom";
 
-export default CreateBankAccount;
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  maxHeight: "100%",
+  overflowY: "auto",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  [theme.breakpoints.up("sm")]: {
+    width: "450px",
+  },
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const CreateBankAccountContainer = styled(Stack)(({ theme }) => ({
+  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+  minHeight: "100%",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4),
+  },
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
+}));
+
+export default function CreateBankAccount(props) {
+  const [accountType, setAccountType] = React.useState("");
+
+  return (
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
+      <CreateBankAccountContainer
+        direction="column"
+        justifyContent="space-between"
+      >
+        <Card variant="outlined">
+          <Typography
+            component="span"
+            variant="h1"
+            sx={(theme) => ({
+              fontSize: "inherit",
+              marginRight: 1,
+              color: "primary.main",
+              ...theme.applyStyles("dark", {
+                color: "primary.light",
+              }),
+            })}
+          >
+            ZENO
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+          >
+            Account Details
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={""}
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="name">Account Name</FormLabel>
+              <TextField
+                autoComplete="name"
+                name="name"
+                required
+                fullWidth
+                id="name"
+                placeholder="Crypto account"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="accountType">Account Type</FormLabel>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={accountType}
+                    label="Account Type"
+                    onChange={(event) => setAccountType(event.target.value)}
+                  >
+                    <MenuItem value={"Current"}>Current</MenuItem>
+                    <MenuItem value={"Default"}>Default</MenuItem>
+                    <MenuItem value={"Savings"}>Savings</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="accountBalance">Account Balance</FormLabel>
+              <TextField
+                required
+                fullWidth
+                id="accountBalance"
+                placeholder="2000000"
+                name="accountBalance"
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="accountCurrency">Account Currency</FormLabel>
+              <TextField
+                required
+                fullWidth
+                name="accountCurrency"
+                placeholder="PKR"
+                id="accountCurrency"
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="bankName">Bank Name</FormLabel>
+              <TextField
+                required
+                fullWidth
+                name="bankName"
+                placeholder="Meezan Bank"
+                id="bankName"
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="accountNumber">Account Number</FormLabel>
+              <TextField
+                required
+                fullWidth
+                name="accountNumber"
+                placeholder="4786235490"
+                id="accountNumber"
+                variant="outlined"
+              />
+            </FormControl>
+            <Button type="submit" fullWidth variant="contained">
+              Create Account
+            </Button>
+          </Box>
+        </Card>
+      </CreateBankAccountContainer>
+    </AppTheme>
+  );
+}
