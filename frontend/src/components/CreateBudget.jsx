3fa9fee8a6b80310ab6fbd9ee9249438486397ca
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-07 15:12:03 +0500

    Create budget form to create budget

--- frontend/src/components/CreateBudget.jsx
+++ frontend/src/components/CreateBudget.jsx
@@ -1,5 +1,198 @@
-const CreateBudget = () => {
-  return <div></div>;
-};
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { styled } from "@mui/material/styles";
+import AppTheme from "./shared-theme/AppTheme";
+import ColorModeSelect from "./shared-theme/ColorModeSelect";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useNavigate } from "react-router-dom";
+import { useCreateUserBudgetMutation } from "../redux/slices/budgetApiSlice";
+import { toast } from "react-toastify";
 
-export default CreateBudget;
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  maxHeight: "100%",
+  overflowY: "auto",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  [theme.breakpoints.up("sm")]: {
+    width: "450px",
+  },
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const CreateBudgetContainer = styled(Stack)(({ theme }) => ({
+  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+  minHeight: "100%",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4),
+  },
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
+}));
+
+export default function CreateBudget(props) {
+  const [budgetAmount, setBudgetAmount] = React.useState("");
+  const [budgetPeriod, setBudgetPeriod] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [startDate, setStartDate] = React.useState(null);
+  const [endDate, setEndDate] = React.useState(null);
+
+  const navigate = useNavigate();
+
+  const [CreateBudget] = useCreateUserBudgetMutation();
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      await CreateBudget({
+        budgetAmount,
+        budgetPeriod,
+        description,
+        startDate,
+        endDate,
+      }).unwrap();
+      navigate("/home");
+      toast.success("Budget created successfully.");
+    } catch (error) {
+      toast.error(error?.data?.message || error.error);
+    }
+  };
+
+  return (
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
+      <CreateBudgetContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography
+            component="span"
+            variant="h1"
+            sx={(theme) => ({
+              fontSize: "inherit",
+              marginRight: 1,
+              color: "primary.main",
+              ...theme.applyStyles("dark", {
+                color: "primary.light",
+              }),
+            })}
+          >
+            ZENO
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+          >
+            Budget Details
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={submitHandler}
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="amount">Budget Amount</FormLabel>
+              <TextField
+                name="amount"
+                required
+                fullWidth
+                id="amount"
+                placeholder="150000"
+                value={budgetAmount}
+                onChange={(event) => setBudgetAmount(event.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="budgetPeriod">Budget Period</FormLabel>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <Select
+                    labelId="budgetPeriod"
+                    id="budgetPeriod"
+                    label="Budget Period"
+                    value={budgetPeriod}
+                    onChange={(event) => setBudgetPeriod(event.target.value)}
+                  >
+                    <MenuItem value={"Weekly"}>Weekly</MenuItem>
+                    <MenuItem value={"Monthly"}>Monthly</MenuItem>
+                    <MenuItem value={"Yearly"}>Yearly</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="description">Description</FormLabel>
+              <TextField
+                name="description"
+                multiline
+                maxRows={4}
+                required
+                fullWidth
+                id="description"
+                placeholder="Budget for shopping in a year"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="startDate">Start Date</FormLabel>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  value={startDate}
+                  onChange={(value) => setStartDate(value)}
+                />
+              </LocalizationProvider>
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="endDate">End Date</FormLabel>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  value={endDate}
+                  onChange={(value) => setEndDate(value)}
+                />
+              </LocalizationProvider>
+            </FormControl>
+            <Button type="submit" fullWidth variant="contained">
+              Create Budget
+            </Button>
+          </Box>
+        </Card>
+      </CreateBudgetContainer>
+    </AppTheme>
+  );
+}
