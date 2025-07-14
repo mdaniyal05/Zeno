abd84558eb2ab1bfe9f73dfb5535b219d8786985
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-14 19:29:01 +0500

    Create logic to generate otp

---
+++ backend/src/controllers/otp.controller.js
@@ -0,0 +1,47 @@
+const otpGenerator = require("otp-generator");
+const asyncHandler = require("express-async-handler");
+const Otp = require("../models/otp.model");
+const User = require("../models/user.model");
+
+const generateOtp = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const userExists = await User.findOne({ where: { email: email } });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists.");
+  }
+
+  let otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  });
+
+  let result = await Otp.findOne({ where: { otp: otp } });
+
+  while (result) {
+    otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    result = await Otp.findOne({ where: { otp: otp } });
+  }
+
+  const newOtp = await Otp.create({
+    email: email,
+    otp: otp,
+  });
+
+  if (newOtp) {
+    res.status(201).json({
+      otp: newOtp.otp,
+      message: "OTP Sent Successfully.",
+    });
+  } else {
+    res.status(500);
+    throw new Error("Something Went Wrong.");
+  }
+});
