f440d20537c835f3ced86ebf535648ef0c0e4e95
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-15 11:45:51 +0500

    Handle otp verification in auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,16 +1,58 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user.model");
+const Otp = require("../models/otp.model");
 const generateJwtToken = require("../utils/generateJwtToken");
 const bcrypt = require("bcryptjs");
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, password } = req.body;
+});
+
+const verifyOtp = asyncHandler(async (req, res) => {
+  const { email, OTP } = req.body;
 
   const userExists = await User.findOne({ where: { email: email } });
 
   if (userExists) {
     res.status(400);
     throw new Error("User Already Exists.");
+  }
+
+  const otp = await Otp.findOne({ where: { email: email } });
+
+  if (otp.isBlocked) {
+    const currentTime = new Date();
+    if (currentTime < otp.blockUntil) {
+      res.status(403);
+      throw new Error("You Are Blocked. Try After Some Time.");
+    } else {
+      otp.isBlocked = false;
+      otp.OtpAttempts = 0;
+    }
+  }
+
+  if (otp.otp !== OTP) {
+    otp.OtpAttempts++;
+
+    if (otp.OtpAttempts >= 3) {
+      otp.isBlocked = true;
+      let blockUntil = new Date();
+      blockUntil.setHours(blockUntil.getHours() + 1);
+      otp.blockUntil = blockUntil;
+    }
+
+    await otp.save();
+
+    res.status(403);
+    throw new Error("Invalid OTP.");
+  }
+
+  const otpCreatedTime = otp.createdAt;
+  const currentTime = new Date();
+
+  if (currentTime - otpCreatedTime > 3 * 60 * 1000) {
+    res.status(403);
+    throw new Error("OTP Expired.");
   }
 
   const newUser = await User.create({
