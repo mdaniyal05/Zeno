c860d2715d18e5c018f3aa4ea40179b20ec81c85
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-16 19:13:18 +0500

    Handle otp verification during sign up

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -14,6 +14,7 @@ import AppTheme from "./shared-theme/AppTheme";
 import ColorModeSelect from "./shared-theme/ColorModeSelect";
 import { useDispatch, useSelector } from "react-redux";
 import { useRegisterMutation } from "../redux/slices/authApiSlice";
+import { useGenerateOtpMutation } from "../redux/slices/otpApiSlice";
 import { setCredentials } from "../redux/slices/authSlice";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -74,6 +75,8 @@ export default function SignUp(props) {
   const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
   const [nameError, setNameError] = React.useState(false);
   const [nameErrorMessage, setNameErrorMessage] = React.useState("");
+  const [otp, setOtp] = React.useState("");
+  const [isMailSent, setIsMailSent] = React.useState(false);
 
   let isValid = true;
 
@@ -81,6 +84,7 @@ export default function SignUp(props) {
   const navigate = useNavigate();
 
   const [register] = useRegisterMutation();
+  const [generateOtp] = useGenerateOtpMutation();
 
   const { userInfo } = useSelector((state) => state.auth);
 
@@ -89,6 +93,26 @@ export default function SignUp(props) {
       navigate("/home");
     }
   }, [navigate, userInfo]);
+
+  const verifyHandler = async (event) => {
+    event.preventDefault();
+
+    if (confirmPassword !== password) {
+      toast.error("Confirm password does not match password.");
+    } else {
+      try {
+        const res = await generateOtp({
+          email,
+        }).unwrap();
+
+        if (res) {
+          setIsMailSent(true);
+        }
+      } catch (error) {
+        toast.error(error?.data?.message || error.error);
+      }
+    }
+  };
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -147,147 +171,212 @@ export default function SignUp(props) {
   };
 
   return (
-    <AppTheme {...props}>
-      <CssBaseline enableColorScheme />
-      <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
-      <SignUpContainer direction="column" justifyContent="space-between">
-        <Card variant="outlined">
-          <Typography
-            component="span"
-            variant="h1"
-            sx={(theme) => ({
-              fontSize: "inherit",
-              marginRight: 1,
-              color: "primary.main",
-              ...theme.applyStyles("dark", {
-                color: "primary.light",
-              }),
-            })}
-          >
-            ZENO
-          </Typography>
-          <Typography
-            component="h1"
-            variant="h4"
-            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
-          >
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={submitHandler}
-            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-          >
-            <FormControl>
-              <FormLabel htmlFor="name">First name</FormLabel>
-              <TextField
-                autoComplete="name"
-                name="name"
-                required
-                fullWidth
-                id="name"
-                placeholder="Jon Snow"
-                value={firstName}
-                onChange={(event) => setFirstName(event.target.value)}
-                error={nameError}
-                helperText={nameErrorMessage}
-                color={nameError ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="name">Last name</FormLabel>
-              <TextField
-                autoComplete="name"
-                name="name"
-                required
-                fullWidth
-                id="name"
-                placeholder="Jon Snow"
-                value={lastName}
-                onChange={(event) => setLastName(event.target.value)}
-                error={nameError}
-                helperText={nameErrorMessage}
-                color={nameError ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                placeholder="your@email.com"
-                name="email"
-                autoComplete="email"
-                variant="outlined"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                error={emailError}
-                helperText={emailErrorMessage}
-                color={passwordError ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                placeholder="••••••"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-                variant="outlined"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                error={passwordError}
-                helperText={passwordErrorMessage}
-                color={passwordError ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="password">Confirm password</FormLabel>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                placeholder="••••••"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-                variant="outlined"
-                value={confirmPassword}
-                onChange={(event) => setConfirmPassword(event.target.value)}
-                error={passwordError}
-                helperText={passwordErrorMessage}
-                color={passwordError ? "error" : "primary"}
-              />
-            </FormControl>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              onClick={validateInputs}
-            >
-              Sign up
-            </Button>
-          </Box>
-          <Divider>
-            <Typography sx={{ color: "text.secondary" }}>or</Typography>
-          </Divider>
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <Typography sx={{ textAlign: "center" }}>
-              Already have an account?{" "}
-              <Link
-                to={"/signin"}
-                style={{ textDecoration: "none", color: "white" }}
+    <>
+      {isMailSent ? (
+        <AppTheme {...props}>
+          <CssBaseline enableColorScheme />
+          <ColorModeSelect
+            sx={{ position: "fixed", top: "1rem", right: "1rem" }}
+          />
+          <SignUpContainer direction="column" justifyContent="space-between">
+            <Card variant="outlined">
+              <Typography
+                component="span"
+                variant="h1"
+                sx={(theme) => ({
+                  fontSize: "inherit",
+                  marginRight: 1,
+                  color: "primary.main",
+                  ...theme.applyStyles("dark", {
+                    color: "primary.light",
+                  }),
+                })}
               >
-                Sign in
-              </Link>
-            </Typography>
-          </Box>
-        </Card>
-      </SignUpContainer>
-    </AppTheme>
+                ZENO
+              </Typography>
+              <Typography
+                component="h1"
+                variant="h4"
+                sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+              >
+                Verify OTP
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={submitHandler}
+                sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+              >
+                <FormControl>
+                  <FormLabel htmlFor="otp">OTP</FormLabel>
+                  <TextField
+                    autoComplete="otp"
+                    name="otp"
+                    required
+                    fullWidth
+                    id="otp"
+                    placeholder="000000"
+                    value={otp}
+                    onChange={(event) => setOtp(event.target.value)}
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  onClick={submitHandler}
+                >
+                  Verify
+                </Button>
+              </Box>
+            </Card>
+          </SignUpContainer>
+        </AppTheme>
+      ) : (
+        <AppTheme {...props}>
+          <CssBaseline enableColorScheme />
+          <ColorModeSelect
+            sx={{ position: "fixed", top: "1rem", right: "1rem" }}
+          />
+          <SignUpContainer direction="column" justifyContent="space-between">
+            <Card variant="outlined">
+              <Typography
+                component="span"
+                variant="h1"
+                sx={(theme) => ({
+                  fontSize: "inherit",
+                  marginRight: 1,
+                  color: "primary.main",
+                  ...theme.applyStyles("dark", {
+                    color: "primary.light",
+                  }),
+                })}
+              >
+                ZENO
+              </Typography>
+              <Typography
+                component="h1"
+                variant="h4"
+                sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+              >
+                Sign up
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={verifyHandler}
+                sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+              >
+                <FormControl>
+                  <FormLabel htmlFor="name">First name</FormLabel>
+                  <TextField
+                    autoComplete="name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    placeholder="Jon Snow"
+                    value={firstName}
+                    onChange={(event) => setFirstName(event.target.value)}
+                    error={nameError}
+                    helperText={nameErrorMessage}
+                    color={nameError ? "error" : "primary"}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="name">Last name</FormLabel>
+                  <TextField
+                    autoComplete="name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    placeholder="Jon Snow"
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}
+                    error={nameError}
+                    helperText={nameErrorMessage}
+                    color={nameError ? "error" : "primary"}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    placeholder="your@email.com"
+                    name="email"
+                    autoComplete="email"
+                    variant="outlined"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    error={emailError}
+                    helperText={emailErrorMessage}
+                    color={passwordError ? "error" : "primary"}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    placeholder="••••••"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    variant="outlined"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    error={passwordError}
+                    helperText={passwordErrorMessage}
+                    color={passwordError ? "error" : "primary"}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="password">Confirm password</FormLabel>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    placeholder="••••••"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    variant="outlined"
+                    value={confirmPassword}
+                    onChange={(event) => setConfirmPassword(event.target.value)}
+                    error={passwordError}
+                    helperText={passwordErrorMessage}
+                    color={passwordError ? "error" : "primary"}
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  onClick={validateInputs}
+                >
+                  Sign up
+                </Button>
+              </Box>
+              <Divider>
+                <Typography sx={{ color: "text.secondary" }}>or</Typography>
+              </Divider>
+              <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                <Typography sx={{ textAlign: "center" }}>
+                  Already have an account?{" "}
+                  <Link
+                    to={"/signin"}
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    Sign in
+                  </Link>
+                </Typography>
+              </Box>
+            </Card>
+          </SignUpContainer>
+        </AppTheme>
+      )}
+    </>
   );
 }
