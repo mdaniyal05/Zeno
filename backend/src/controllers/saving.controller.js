d0b7d0aac11786a17f8a58cd803904cd555d1cc9
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-30 13:32:34 +0500

    Create get user saving controller function

--- backend/src/controllers/saving.controller.js
+++ backend/src/controllers/saving.controller.js
@@ -1,6 +1,25 @@
 const asyncHandler = require("express-async-handler");
+const Saving = require("../models/saving.model");
 
-const getUserSaving = asyncHandler(async (req, res) => {});
+const getUserSaving = asyncHandler(async (req, res) => {
+  const savingId = req.params.id;
+  const saving = await Saving.findByPk(savingId);
+
+  if (saving) {
+    res.status(200).json({
+      savingId: saving.savingId,
+      title: saving.title,
+      targetAmount: saving.targetAmount,
+      currentAmount: saving.currentAmount,
+      description: saving.description,
+      status: saving.status,
+      accountId: saving.accountId,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Saving not found.");
+  }
+});
 
 const getAllUserSavings = asyncHandler(async (req, res) => {});
 
