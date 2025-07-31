717e2f98c8b626ac3e49a77a93cf922de95f6cf2
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-07-31 17:21:46 +0500

    Create form to update savings

---
+++ frontend/src/components/UpdateSaving.jsx
@@ -0,0 +1,208 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { styled } from "@mui/material/styles";
+import AppTheme from "./shared-theme/AppTheme";
+import ColorModeSelect from "./shared-theme/ColorModeSelect";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  useGetUserSavingQuery,
+  useUpdateUserSavingMutation,
+} from "../redux/slices/savingApiSlice";
+import { useGetAllUserAccountsQuery } from "../redux/slices/bankAccountApiSlice";
+import { toast } from "react-toastify";
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  maxHeight: "100%",
+  overflowY: "auto",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  [theme.breakpoints.up("sm")]: {
+    width: "450px",
+  },
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const UpdateSavingContainer = styled(Stack)(({ theme }) => ({
+  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+  minHeight: "100%",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4),
+  },
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
+}));
+
+export default function UpdateSaving(props) {
+  const { id } = useParams();
+
+  const { data: savingData } = useGetUserSavingQuery(id);
+  const { data: accountData } = useGetAllUserAccountsQuery();
+
+  const [title, setTitle] = React.useState("");
+  const [targetAmount, setTargetAmount] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [accountId, setAccountId] = React.useState("");
+
+  const navigate = useNavigate();
+
+  const [updateSaving] = useUpdateUserSavingMutation();
+
+  React.useEffect(() => {
+    setTitle((savingData && savingData.title) || "");
+    setTargetAmount((savingData && savingData.targetAmount) || "");
+    setDescription((savingData && savingData.description) || "");
+    setAccountId((savingData && savingData.accountId) || "");
+  }, [savingData]);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      await updateSaving({
+        id: id,
+        title,
+        targetAmount,
+        description,
+        accountId,
+      }).unwrap();
+      navigate("/home");
+      toast.success("Saving updated successfully.");
+    } catch (error) {
+      toast.error(error?.data?.message || error.error);
+    }
+  };
+
+  return (
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
+      <UpdateSavingContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography
+            component="span"
+            variant="h1"
+            sx={(theme) => ({
+              fontSize: "inherit",
+              marginRight: 1,
+              color: "primary.main",
+              ...theme.applyStyles("dark", {
+                color: "primary.light",
+              }),
+            })}
+          >
+            ZENO
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+          >
+            Update Saving
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={submitHandler}
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="title">Title</FormLabel>
+              <TextField
+                name="title"
+                required
+                fullWidth
+                id="title"
+                placeholder="Emergency Funds"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="targetAmount">Savings Target</FormLabel>
+              <TextField
+                required
+                fullWidth
+                id="targetAmount"
+                placeholder="7000000"
+                name="targetAmount"
+                variant="outlined"
+                value={targetAmount}
+                onChange={(event) => setTargetAmount(event.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="description">Description</FormLabel>
+              <TextField
+                name="description"
+                required
+                fullWidth
+                id="description"
+                placeholder="Savings for emergency situations"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="savingAccount">Saving Account</FormLabel>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <Select
+                    labelId="savingAccount"
+                    id="savingAccount"
+                    label="Saving Account"
+                    value={accountId}
+                    onChange={(event) => setAccountId(event.target.value)}
+                  >
+                    {accountData &&
+                      accountData.accountsData
+                        .filter((account) => account.accountType === "Savings")
+                        .map((account) => (
+                          <MenuItem value={account.accountId}>
+                            {account.accountName}
+                          </MenuItem>
+                        ))}
+                  </Select>
+                </FormControl>
+              </Box>
+            </FormControl>
+            <Button type="submit" fullWidth variant="contained">
+              Update
+            </Button>
+          </Box>
+        </Card>
+      </UpdateSavingContainer>
+    </AppTheme>
+  );
+}
