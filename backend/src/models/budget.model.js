6cc3637a545542c631d198645b1fc1d1d55f3212
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-02 19:53:25 +0500

    Create budget model

---
+++ backend/src/models/budget.model.js
@@ -0,0 +1,80 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../db/db");
+const User = require("./user.model");
+
+const Budget = sequelize.define(
+  "Budget",
+  {
+    budgetId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    startDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        validator(value) {
+          const currentDate = new Date();
+
+          if (value <= currentDate) {
+            throw new Error("Start date must be in future.");
+          }
+        },
+      },
+    },
+    endDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        validator(value) {
+          if (value < this.startDate) {
+            throw new Error("End date must be after the start date.");
+          }
+        },
+      },
+    },
+    budgetAmount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    amountSpent: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    amountRemaining: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [0, 150],
+          msg: "Description cannot exceed 150 characters.",
+        },
+      },
+    },
+    status: {
+      type: DataTypes.ENUM("Active", "Completed", "Exceeded"),
+    },
+  },
+  {
+    tableName: "Budgets",
+    timestamps: true,
+  }
+);
+
+User.hasMany(Budget, {
+  foreignKey: { name: "userId", allowNull: false },
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+Budget.belongsTo(User, {
+  as: "owner",
+  foreignKey: { name: "userId" },
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+module.exports = Budget;
