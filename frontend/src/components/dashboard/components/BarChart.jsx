d266974684b3217e45588324ca5403d946353df3
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-08 19:04:26 +0500

    Revert dashboard

--- frontend/src/components/dashboard/components/BarChart.jsx
+++ frontend/src/components/dashboard/components/BarChart.jsx
@@ -1,43 +1,7 @@
-import * as React from "react";
-import { BarChart } from "@mui/x-charts/BarChart";
-import { useGetAllUserIncomesQuery } from "../../../redux/slices/incomeApiSlice";
-import { useGetAllUserExpensesQuery } from "../../../redux/slices/expenseApiSlice";
-import { dataset } from "../../../lib/dataset";
-import incomeMap from "../../../lib/incomeMapData";
-import expenseMap from "../../../lib/expenseMapData";
+import React from "react";
 
-export default function IncomeAndExpenseBarChart() {
-  const { data: income } = useGetAllUserIncomesQuery();
-  const { data: expense } = useGetAllUserExpensesQuery();
+const BarChart = () => {
+  return <div>Barchart</div>;
+};
 
-  React.useEffect(() => {
-    if (income && expense) {
-      incomeMap(dataset, income);
-      expenseMap(dataset, expense);
-    }
-  }, [income, expense]);
-
-  function valueFormatter(value) {
-    return `${value} PKR`;
-  }
-
-  const chartSetting = {
-    xAxis: [{ label: "Amount" }],
-    height: 400,
-    margin: { left: 0 },
-  };
-
-  return (
-    <BarChart
-      dataset={dataset}
-      yAxis={[{ scaleType: "band", dataKey: "month" }]}
-      series={[
-        { dataKey: "income", label: "Total monthly income", valueFormatter },
-        { dataKey: "expense", label: "Total monthly expense", valueFormatter },
-      ]}
-      layout="horizontal"
-      grid={{ vertical: true }}
-      {...chartSetting}
-    />
-  );
-}
+export default BarChart;
