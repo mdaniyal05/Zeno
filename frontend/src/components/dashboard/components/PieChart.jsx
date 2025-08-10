5127a331d71efc1a2a19df659269f11eeab93d9a
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-10 21:14:56 +0500

    Create a combined dataset for pie chart

--- frontend/src/components/dashboard/components/PieChart.jsx
+++ frontend/src/components/dashboard/components/PieChart.jsx
@@ -1,16 +1,44 @@
 import * as React from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
+import { useGetDashboardDataQuery } from "../../../redux/slices/dashboardApiSlice";
 
 export default function Piechart() {
+  const [dataset, setDataset] = React.useState([]);
+  const { data } = useGetDashboardDataQuery();
+
+  React.useEffect(() => {
+    if (data) {
+      const combined = [
+        ...data.totalIncomeData.map((obj, idx) => ({
+          id: idx + 1,
+          value: obj.allIncome,
+          label: "Total income",
+        })),
+        ...data.totalExpenseData.map((obj, idx) => ({
+          id: data.totalIncomeData.length + idx + 1,
+          value: obj.allExpense,
+          label: "Total expense",
+        })),
+        ...data.totalSavingData.map((obj, idx) => ({
+          id:
+            data.totalIncomeData.length +
+            data.totalExpenseData.length +
+            idx +
+            1,
+          value: obj.allSaving,
+          label: "Total saving",
+        })),
+      ];
+
+      setDataset(combined);
+    }
+  }, [data]);
+
   return (
     <PieChart
       series={[
         {
-          data: [
-            { id: 0, value: 10, label: "series A" },
-            { id: 1, value: 15, label: "series B" },
-            { id: 2, value: 20, label: "series C" },
-          ],
+          data: dataset,
           innerRadius: 30,
           outerRadius: 100,
           paddingAngle: 5,
