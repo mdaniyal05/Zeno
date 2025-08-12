17afa33e464e055ab16c0c5b8b0daac921163b47
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-13 00:05:05 +0500

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,8 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer, Flip } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useLogoutMutation } from "./redux/slices/authApiSlice";
-import { logout } from "./redux/slices/authSlice";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 function App() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const [logoutApiCall] = useLogoutMutation();
-
-  const clearStorage = async () => {
-    let session = sessionStorage.getItem("register");
-
-    if (session == null) {
-      try {
-        await logoutApiCall().unwrap();
-        dispatch(logout());
-        navigate("/");
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    sessionStorage.setItem("register", 1);
-  };
-
-  window.addEventListener("load", clearStorage);
-
   return (
     <>
       <ToastContainer
