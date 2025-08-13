db91fc1d284f2871053290c235662856e9088836
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-13 15:34:16 +0500

    Fix

--- backend/src/utils/generateJwtToken.js
+++ backend/src/utils/generateJwtToken.js
@@ -1,16 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const generateJwtToken = (payload, secret, expiry) => {
-  const jwtToken = jwt.sign({ payload }, secret, {
+  const token = jwt.sign({ payload }, secret, {
     expiresIn: expiry,
   });
 
-  // res.cookie("jwtToken", jwtToken, {
-  //   httpOnly: true,
-  //   maxAge: 3600000,
-  //   sameSite: "strict",
-  //   secure: process.env.NODE_ENV !== "development",
-  // });
+  return token;
 };
 
 module.exports = generateJwtToken;
