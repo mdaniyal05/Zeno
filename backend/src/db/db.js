eb044bf2bb4edacd76017198f12c1ed94da4098e
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-23 15:42:35 +0500

    Change DATABASE from MS SQL server to POSTGRES

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,13 +4,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USERNAME,
-  process.env.PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.HOST,
-    port: process.env.SQL_PORT,
-    dialect: process.env.DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
   }
 );
 
