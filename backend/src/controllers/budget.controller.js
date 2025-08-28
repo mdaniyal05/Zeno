de2389f99eed943bcb28cdf5c3665d2a36510a93
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-08-28 08:55:09 +0500

    Complete refactor and fix budget controllers

--- backend/src/controllers/budget.controller.js
+++ backend/src/controllers/budget.controller.js
@@ -1,40 +1,63 @@
 const asyncHandler = require("express-async-handler");
 const Budget = require("../models/budget.model");
 
+/*
+
+Get single budget controller
+
+*/
+
 const getUserBudget = asyncHandler(async (req, res) => {
   const budgetId = req.params.id;
   const budget = await Budget.findByPk(budgetId);
 
-  if (budget) {
-    res.status(200).json({
-      budgetId: budget.budgetId,
-      startDate: budget.startDate,
-      endDate: budget.endDate,
-      budgetAmount: budget.budgetAmount,
-      amountSpent: budget.amountSpent,
-      amountRemaining: budget.amountRemaining,
-      description: budget.description,
-      status: budget.status,
-    });
-  } else {
+  if (!budget) {
     res.status(404);
     throw new Error("Budget not found.");
   }
+
+  res.status(200).json({
+    ...budget.toJSON(),
+  });
 });
+
+/*
+
+Get single budget controller (END)
+
+*/
+
+/*
+
+Get all budget controller
+
+*/
 
 const getAllUserBudgets = asyncHandler(async (req, res) => {
   const userId = req.user.userId;
   const budgets = await Budget.findAll({ where: { userId: userId } });
 
-  if (budgets) {
-    res.status(200).json({
-      budgetsData: budgets,
-    });
-  } else {
+  if (!budgets) {
     res.status(404);
-    throw new Error("No budgets available.");
+    throw new Error("No budgets found.");
   }
+
+  res.status(200).json({
+    budgetsData: budgets,
+  });
 });
+
+/*
+
+Get all budget controller (END)
+
+*/
+
+/*
+
+Create budget controller
+
+*/
 
 const createUserBudget = asyncHandler(async (req, res) => {
   const { startDate, endDate, budgetAmount, description } = req.body;
@@ -46,87 +69,145 @@ const createUserBudget = asyncHandler(async (req, res) => {
 
   if (budgetAmount <= 0) {
     res.status(400);
-    throw new Error("Negative values and zero are not allowed.");
+    throw new Error("Budget amount must be greater than 0.");
   }
 
   const userId = req.user.userId;
 
-  const budget = await Budget.findOne({
-    where: { status: "Active", userId: userId },
-  });
+  const t = await sequelize.transaction();
 
-  if (budget) {
-    res.status(400);
-    throw new Error("You can only have one active budget at a time.");
-  }
-
-  const newBudget = await Budget.create({
-    startDate: startDate,
-    endDate: endDate,
-    budgetAmount: budgetAmount,
-    amountRemaining: budgetAmount,
-    description: description,
-    userId: userId,
-  });
-
-  if (newBudget) {
-    res.status(201).json({
-      message: "Budget created successfully.",
+  try {
+    const budget = await Budget.findOne({
+      where: { status: "Active", userId: userId },
+      transaction: t,
     });
-  } else {
+
+    if (budget) {
+      res.status(400);
+      throw new Error("You can only have one active budget at a time.");
+    }
+
+    const newBudget = await Budget.create(
+      {
+        startDate: startDate,
+        endDate: endDate,
+        budgetAmount: budgetAmount,
+        amountRemaining: budgetAmount,
+        description: description,
+        userId: userId,
+      },
+      { transaction: t }
+    );
+
+    if (newBudget) {
+      await t.commit();
+
+      res.status(201).json({
+        ...newBudget.toJSON(),
+        message: "Budget created successfully.",
+      });
+    }
+  } catch (error) {
+    await t.rollback();
     res.status(400);
-    throw new Error("Invalid budget data.");
+    throw new Error(error.message);
   }
 });
+
+/*
+
+Create budget controller (END)
+
+*/
+
+/*
+
+Update budget controller
+
+*/
 
 const updateUserBudget = asyncHandler(async (req, res) => {
   const budgetId = req.params.id;
   const budget = await Budget.findByPk(budgetId);
 
-  if (budget) {
-    if (req.body.budgetAmount <= 0) {
-      res.status(400);
-      throw new Error("Negative values and zero are not allowed.");
-    }
-
-    if (req.body.budgetAmount !== budget.budgetAmount) {
-      budget.amountRemaining = req.body.budgetAmount - budget.amountSpent;
-    }
-
-    budget.startDate = req.body.startDate || budget.startDate;
-    budget.endDate = req.body.endDate || budget.endDate;
-    budget.budgetAmount = req.body.budgetAmount || budget.budgetAmount;
-    budget.description = req.body.description || budget.description;
-
-    const updatedBudget = await budget.save();
-
-    res.status(200).json({
-      startDate: updatedBudget.startDate,
-      endDate: updatedBudget.endDate,
-      budgetAmount: updatedBudget.budgetAmount,
-      description: updatedBudget.description,
-      message: "Budget updated successfully.",
-    });
-  } else {
+  if (!budget) {
     res.status(404);
     throw new Error("Budget not found.");
   }
+
+  if (req.body.budgetAmount <= 0) {
+    res.status(400);
+    throw new Error("Budget amount must be greater than 0.");
+  }
+
+  const t = await sequelize.transaction();
+
+  try {
+    Object.assign(budget, {
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      budgetAmount: req.body.budgetAmount,
+      description: req.body.description,
+    });
+
+    await budget.save({ transaction: t });
+
+    await t.commit();
+
+    res.status(200).json({
+      ...budget.toJSON(),
+      message: "Budget updated successfully.",
+    });
+  } catch (error) {
+    await t.rollback();
+    res.status(400);
+    throw new Error(error.message);
+  }
 });
+
+/*
+
+Update budget controller (END)
+
+*/
+
+/*
+
+Delete budget controller
+
+*/
 
 const deleteUserBudget = asyncHandler(async (req, res) => {
   const budgetId = req.params.id;
   const budget = await Budget.findByPk(budgetId);
 
-  if (budget) {
-    await Budget.destroy({ where: { budgetId: budgetId } });
-    res.status(200).json({
-      message: `Budget deleted successfully.`,
-    });
-  } else {
+  if (!budget) {
     res.status(404);
     throw new Error("Budget not found.");
   }
+
+  const t = await sequelize.transaction();
+
+  try {
+    await budget.destroy({ transaction: t });
+
+    await t.commit();
+
+    res.status(200).json({
+      message: `Budget deleted successfully.`,
+    });
+  } catch (error) {
+    await t.rollback();
+    res.status(400);
+    throw new Error(error.message);
+  }
 });
+
+/*
+
+Delete budget controller (END)
+
+*/
 
 module.exports = {
   getUserBudget,
