7829b94af960d1a54d8aec78d3c58e73b02d85ad
Muhammad Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-09-10 00:05:34 +0500

    Fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -10,6 +10,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+  partitioned: true,
 };
 
 /*
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -6,6 +6,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+  partitioned: true,
 };
 
 /*
