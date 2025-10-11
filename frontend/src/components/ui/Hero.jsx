06eeb7d45f7d6e67b30d04deab16d1c6b7a0ff0f
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-10-12 03:42:31 +0500

    Add dashboard on landing page + Refactor

--- frontend/src/components/ui/Hero.jsx
+++ frontend/src/components/ui/Hero.jsx
@@ -4,6 +4,32 @@ import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+
+const StyledBox = styled("div")(({ theme }) => ({
+  alignSelf: "center",
+  width: "100%",
+  height: 400,
+  marginTop: theme.spacing(8),
+  borderRadius: (theme.vars || theme).shape.borderRadius,
+  outline: "6px solid",
+  outlineColor: "hsla(220, 25%, 80%, 0.2)",
+  border: "1px solid",
+  borderColor: (theme.vars || theme).palette.grey[200],
+  boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
+  backgroundImage: `url(/dashboard2.png)`,
+  backgroundSize: "cover",
+  [theme.breakpoints.up("sm")]: {
+    marginTop: theme.spacing(10),
+    height: 700,
+  },
+  ...theme.applyStyles("dark", {
+    boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
+    backgroundImage: `url(/dashboard1.png)`,
+    outlineColor: "hsla(220, 20%, 42%, 0.1)",
+    borderColor: (theme.vars || theme).palette.grey[700],
+  }),
+}));
 
 export default function Hero() {
   return (
@@ -77,11 +103,11 @@ export default function Hero() {
               direction={{ xs: "column", sm: "row" }}
               spacing={1}
               useFlexGap
-              sx={{ pt: 2, width: { xs: "100%", sm: "350px" } }}
+              sx={{ pt: 2, width: { xs: "100%", sm: "150px" } }}
             >
               <Button
-                variant="contained"
-                color="primary"
+                variant="outlined"
+                color="secondary"
                 size="small"
                 fullWidth
                 sx={{ minWidth: "fit-content" }}
@@ -91,6 +117,7 @@ export default function Hero() {
             </Stack>
           </Link>
         </Stack>
+        <StyledBox id="image" />
       </Container>
     </Box>
   );
