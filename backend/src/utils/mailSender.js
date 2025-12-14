96616c32bc8376879762cffc982340d9a6bfb4eb
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-12-14 08:25:14 +0500

    Make nodemailer as email service fro now

--- backend/src/utils/mailSender.js
+++ backend/src/utils/mailSender.js
@@ -1,34 +1,22 @@
-const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
 
-dotenv.config();
-
-const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
-
-const mailerSend = new MailerSend({
-  apiKey: process.env.MAILERSEND_API_KEY,
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_APP_PASS,
+  },
 });
 
 const mailSender = async (email, subject, text, html) => {
   try {
-    const sentFrom = new Sender(
-      `${process.env.MAIL_SENDER}`,
-      "Zeno Finance Tracker"
-    );
-
-    const recipients = [new Recipient(email, email)];
-
-    const emailParams = new EmailParams()
-      .setFrom(sentFrom)
-      .setTo(recipients)
-      .setSubject(subject)
-      .setHtml(html)
-      .setText(text);
-
-    const response = await mailerSend.email.send(emailParams);
-
-    if (response) {
-      console.log(response);
-    }
+    await transporter.sendMail({
+      from: `"Zeno" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: subject,
+      html: html,
+      text: text,
+    });
   } catch (error) {
     console.error(error);
   }
@@ -36,26 +24,3 @@ const mailSender = async (email, subject, text, html) => {
 
 module.exports = mailSender;
 
-// const sgMail = require("@sendgrid/mail");
-
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const mailSender = async (email, subject, text, html) => {
-//   try {
-//     const message = {
-//       to: email,
-//       from: process.env.MAIL_SENDER,
-//       subject: subject,
-//       text: text,
-//       html: html,
-//     };
-
-//     await sgMail.send(message);
-
-//     console.log("Email sent");
-//   } catch (error) {
-//     console.error("Unable to send email: ", error);
-//   }
-// };
-
-// module.exports = mailSender;
